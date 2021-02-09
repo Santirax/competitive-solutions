1ae48c2eed3e43c51978dd58e5af2f21ab930bf7
Santirax <56557904+Santirax@users.noreply.github.com>
2021-02-08 20:11:11 -0600

    Add files via upload

--- CodeForces/vusTheCossackAndNumbers.cpp
+++ CodeForces/vusTheCossackAndNumbers.cpp
@@ -1,52 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool esDoble(double n){
-  if(n - (int)n != 0)return true;
-  return false;
+bool isInt(double x){
+    double aux = x - (long long)x;
+    if(aux == 0)return true;
+    
+    return false;
 }
 
 int main (){
- int n;
- cin>>n;
- vector<double>num(n);
- int sum = 0;
- //int pos = 0, neg = 0;
- for(int i = 0; i < n; i++){
-  cin>>num[i];
-  int temp = (int)num[i];
-  sum += temp;
-  //cout<<temp<<" y "<<sum<<"\n";
-  //if(num[i] >= 0)pos++; else neg++;
- }
- //cout<<sum<<endl<<endl;
-
- vector<int>res(n);
- if(sum > 0){
-   for(int i = 0; i < n; i++){
-     if(num[i] < 0 && sum != 0 && esDoble(num[i])){
-        res[i] = (int)num[i] - 1;
-        sum--;
-     }else{
-       res[i] = (int)num[i];
-     }
-   }
- }else if(sum < 0){
-   for(int i = 0; i < n; i++){
-      if(num[i] > 0 && sum != 0 && esDoble(num[i])){
-        res[i] = (int)num[i] + 1;
-        sum++;
-      }else{
-        res[i] = num[i];
-      }
-   }
- }else{
-   for(int i = 0; i < n; i++)res[i] = (int)(num[i]);
- }
-
-
- //cout<<sum<<endl;
- for(int x: res)cout<<x<<"\n";
-
- return 0;
+    
+    int n; cin>>n;
+    vector<double> nums(n);
+    long long sum = 0;
+    
+    for(double & x: nums){
+        cin>>x;
+        long long aux = (long long)x;
+        sum += aux;
+    }
+    
+    //cout << "SUma = " << sum << endl;
+    
+    if(sum == 0.0){ //Caso en el que sea cero desde el inicio
+        for(double x: nums){
+            cout << (long long)x << "\n";
+            
+        }
+    } else {
+        
+        if(sum > 0.0){ //Caso positivo
+            
+            for(double & x: nums){
+                if(x < 0 && !isInt(x)){
+                    x = x-1;
+                    sum--;
+                    if(sum == 0)break;
+                }
+            }
+            
+        }else { //Caso negativo
+            
+            for(double & x: nums){
+                if(x > 0 && !isInt(x)){
+                    x = x+1;
+                    sum++;
+                    if(sum == 0)break;
+                }
+            }
+        }
+        
+        for(double x: nums){
+            cout << (long long)x << "\n";
+        }
+    }
+    
+    
+    
+    
+    
+    return 0;
 }
